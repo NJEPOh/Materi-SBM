b4fde1ff9f244bc6b4fb614b0a6b104225e23f02
NJEPOh <hafidzalifr8899@gmail.com>
2025-11-11 20:20:04 +0700

    benerin nama file lagi

--- Guru/Studi Kasus2.py
+++
@@ -1,7 +0,0 @@
-total = int(input("Masukkan total belanja: "))
-if total > 200000:
-    diskon = total * 0.15
-    total_bayar = total - diskon
-else:
-    total_bayar = total
-print("Total yang harus dibayar:", total_bayar)
